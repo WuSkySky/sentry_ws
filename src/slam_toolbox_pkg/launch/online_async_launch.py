04bedd33b9987a4129cf4a7f57749a9d3612814e
Wu Tianhao <1873884053@qq.com>
2026-03-07 15:10:03 +0800

    添加slam_toolbox的slam方案

---
+++ src/slam_toolbox_pkg/launch/online_async_launch.py
@@ -0,0 +1,34 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=os.path.join(get_package_share_directory("slam_toolbox_pkg"),
+                                   'config', 'slam_toolbox_pkg.yaml'),
+    )
+
+    start_async_slam_toolbox_node = Node(
+        parameters=[
+          slam_params_file,
+          {'use_sim_time': False}
+        ],
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen')
+
+    ld = LaunchDescription()
+
+    ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(start_async_slam_toolbox_node)
+
+    return ld
